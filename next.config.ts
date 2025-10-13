974f7eab28efa8688060ee6e144687fd4f820540
jasmi <jpilav1@etf.unsa.ba>
2025-10-13 03:05:46 +0200

    config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    // Ignoriši greške tokom build-a
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
